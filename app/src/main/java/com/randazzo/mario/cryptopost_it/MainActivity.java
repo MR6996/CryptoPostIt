a385e3d80c9a1de8343e2c922c43d019c941fe6f
Pippo paperino <randazzo6996@live.it>
2018-11-24 23:42:16 +0100

    Create navigation architecture

--- app/src/main/java/com/randazzo/mario/cryptopost_it/MainActivity.java
+++ app/src/main/java/com/randazzo/mario/cryptopost_it/MainActivity.java
@@ -1,19 +1,46 @@
 package com.randazzo.mario.cryptopost_it;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.math.BigInteger;
-
-import cryptography.RSAHacker;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDrawerLayout = findViewById(R.id.menu_drawer);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+
+                switch (item.getItemId()) {
+                    case R.id.bezout:
+                        break;
+
+                    case R.id.discret_log:
+                        break;
+
+                    case R.id.rsa:
+                        break;
+
+                    case R.id.el_gamal:
+                        break;
+                }
+
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 }
