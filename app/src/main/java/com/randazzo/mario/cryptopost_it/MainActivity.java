903196210f9baf13df75798b826aaf0b6c396e50
Pippo paperino <randazzo6996@live.it>
2018-11-24 22:05:26 +0100

    Initial commit

---
+++ app/src/main/java/com/randazzo/mario/cryptopost_it/MainActivity.java
@@ -0,0 +1,17 @@
+package com.randazzo.mario.cryptopost_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.math.BigInteger;
+
+import cryptography.RSAHacker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
