0858670ffe65a1d4a6da70bc1b77c3993d3ffa9a
Pippo paperino <randazzo6996@live.it>
2018-11-25 19:55:51 +0100

    Implemented bezout fragment

--- app/src/main/java/com/randazzo/mario/cryptopost_it/BezoutFragment.java
+++ app/src/main/java/com/randazzo/mario/cryptopost_it/BezoutFragment.java
@@ -6,21 +6,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.math.BigInteger;
+
+import cryptography.Utils;
+import io.github.kexanie.library.MathView;
 
 
 public class BezoutFragment extends Fragment {
 
+    private MathView mResultView;
+    private EditText mAEditText;
+    private EditText mBEditText;
 
     public BezoutFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bezout, container, false);
+        View mainView = inflater.inflate(R.layout.fragment_bezout, container, false);
+
+        mResultView = mainView.findViewById(R.id.bezout_result_view);
+        mAEditText = mainView.findViewById(R.id.a_edit_text);
+        mBEditText = mainView.findViewById(R.id.b_edit_text);
+
+        Button calculateButton = mainView.findViewById(R.id.calculate_button);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BigInteger a = new BigInteger(mAEditText.getText().toString());
+                BigInteger b = new BigInteger(mBEditText.getText().toString());
+                BigInteger[] results = Utils.Bezout(a, b);
+                String resultText = String.format(getResources().getString(R.string.bezout_result),
+                        results[0].toString(), results[1].toString());
+                mResultView.setText(resultText);
+            }
+        });
+
+        return mainView;
     }
 
 }
